60ee5c63b5afb89d3a87445632e957412821d193
alangt22 <alansilva2896@gmail.com>
2024-12-11 04:01:02 -0300

    Backend update

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ app.use(cors({
 app.use(express.json());
 
 
-app.use('images', express.static(path.join(__dirname, 'public/images')));
+app.use('public/images', express.static(path.join(__dirname, 'public/images')));
 
 // Rotas
 const UserRoutes = require('./routes/UserRoutes');
