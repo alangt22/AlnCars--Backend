e18857e59ba00609e847618f45ab51b28599089e
alangt22 <alansilva2896@gmail.com>
2024-12-11 01:55:14 -0300

    Backend update

--- db/conn.js
+++ db/conn.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose')
 
 async function main() {
-  await mongoose.connect('mongodb+srv://alan:123@cluster0.zk0d5.mongodb.net/?retryWrites=true&w=majority&appName=Cluster00')
+  await mongoose.connect('mongodb+srv://alan:123@cluster0.zk0d5.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0')
   console.log('Conectou com Mongoose!')
 }
 
