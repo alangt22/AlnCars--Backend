6704521d699a4e950eeec309d44ea2ed4dd08ce2
alangt22 <alansilva2896@gmail.com>
2024-12-11 01:47:18 -0300

    Backend update

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express();
 
 
 app.use(cors({
-  origin: 'http://localhost:5173', 
+  origin: 'https://aln-cars.netlify.app/', 
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true, 
